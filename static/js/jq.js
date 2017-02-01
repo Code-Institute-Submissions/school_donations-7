5a379bd981b2f23c7a0aa815c199c675aa15788f
RandyMarsh288 <jonhill356@gmail.com>
2017-02-01 16:51:49 +0000

    opening text toggles, default hidden

---
+++ static/js/jq.js
@@ -0,0 +1,15 @@
+//waits until page is loaded first
+$(document).ready(function(){
+
+    $("#hider1").click(function(){
+        $("#hidden1").slideToggle();
+    });
+
+    $("#hider2").click(function(){
+        $("#hidden2").slideToggle();
+    });
+
+    $("#hider3").click(function(){
+        $("#hidden3").slideToggle();
+    });
+});
